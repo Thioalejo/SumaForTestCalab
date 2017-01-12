31456f0aa0c2ffff12bd5518a6def07b4123d966
Alejandro <alejo.saraza@gmail.com>
2017-01-12 17:09:39 -0500

    Se empezo a implemntar el caso para probar la aplicacion, aun no funciona desconozco el error!!!

--- SumaUITest/Tests.cs
+++ SumaUITest/Tests.cs
@@ -8,7 +8,7 @@ using Xamarin.UITest.Queries;
 namespace SumaUITest
 {
     [TestFixture(Platform.Android)]
-    [TestFixture(Platform.iOS)]
+    //[TestFixture(Platform.iOS)]
     public class Tests
     {
         IApp app;
@@ -29,6 +29,28 @@ namespace SumaUITest
         public void AppLaunches()
         {
             app.Screenshot("First screen.");
+        }
+
+        [Test]
+        public void SumaDeDosnumerosEnteros()
+        {
+            //arrange
+            app.EnterText("EntryA", "5");
+            app.EnterText("EntryA", "5");
+
+            //act
+
+            //para presionar el boton
+
+            app.Tap("btnCalcularA");
+
+
+            //Assert
+            //para guardar dato tomado 
+            var appResult = app.Query("EntryR").First(resul => resul.Text == "10");
+            Assert.IsTrue(appResult != null, "El resultado del label no es correcto");
+
+
         }
     }
 }
