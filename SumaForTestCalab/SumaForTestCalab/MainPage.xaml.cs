a5e7dc619c417f01d016b836e84f68f601e479cb
Alejandro <alejo.saraza@gmail.com>
2017-01-11 21:41:48 -0500

    Carga inicial suma basica hecha

---
+++ SumaForTestCalab/SumaForTestCalab/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SumaForTestCalab
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            btnSumar.Clicked += BtnSumar_Clicked;
+        }
+
+        private void BtnSumar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                int num1 = Convert.ToInt32(txtNumero1.Text);
+                int num2 = Convert.ToInt32(txtNumero1.Text);
+                txtResultado.Text = Sumar(num1, num2).ToString();
+            }
+            catch (Exception)
+            {
+                DisplayAlert("Alert", "Ingreso un dato invalido", "OK");
+            }
+            
+            
+        }
+
+
+        public int Sumar(int a, int b)
+        {
+            int suma = a + b;
+            return suma;
+        }
+    }
+
+}
